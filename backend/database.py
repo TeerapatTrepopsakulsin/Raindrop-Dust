4079ba653f4bad99de166b96fc91a12a38bfb44d
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2025-04-09 17:11:28 +0700

    Add Group by Hourly mechanics

--- backend/database.py
+++ backend/database.py
@@ -1,19 +1,26 @@
 import os
-from sqlalchemy import create_engine, func
-from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import create_engine, func, text
+
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from sqlalchemy.ext.declarative import declarative_base
+from utils import DELETE_1, GROUP_HOURLY_SQL_STATEMENT
+Base = declarative_base()
 
 load_dotenv()
 DB_URL = os.getenv("DB_URL")
 engine = create_engine(DB_URL,echo=True)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
 
-Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
     try :
+        db.execute(text(DELETE_1))
+        db.commit()
+        db.execute(text(GROUP_HOURLY_SQL_STATEMENT))
+        db.commit()
         yield db
     finally:
         db.close()
