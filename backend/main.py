99fc612883d4eb23b10a2aa1f3c2f6cbb0309d8c
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2025-04-10 09:41:17 +0700

    API endpoint /data/latest

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,13 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
+@app.get("/data/latest", response_model=list[schemas.Hourly])
+async def get_latest_data(limit:int=1, db: Session = Depends(get_db)):
+    data = crud.get_hourly(db, limit=limit)
+    data = schematise_hourly_response(data)
+    return data
+
+
 @app.get("/data")
 async def get_data(skip:int=0, limit:int=1, db:Session=Depends(get_db)):
     weathers = crud.get_hourly(db, skip=skip, limit=limit)
