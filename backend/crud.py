f06262115db52d9811268585ab95052be69e5b5a
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2025-04-10 09:28:15 +0700

    Create get_hourly for hourly table

--- backend/crud.py
+++ backend/crud.py
@@ -1,7 +1,6 @@
 from sqlalchemy.orm import Session
 
 from models import Weather, KidBright, Hourly
-import schemas
 
 
 def get_weather(db: Session, _id: int):
@@ -11,4 +10,5 @@ def get_weathers(db: Session, skip:int=0, limit:int=100):
     return db.query(Weather).offset(skip).limit(limit).all()
 
 def get_hourly(db: Session, skip:int=0, limit:int=100):
-    return db.query(Hourly).offset(skip).limit(limit).all()
+    return db.query(Hourly).order_by(Hourly.ts.desc()).offset(skip).limit(limit).all()
+
