e77cd5d58c42661c8ace92063c9a9c63169870eb
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2025-04-16 10:48:36 +0700

    Add Current and Exploration pages

--- frontend/pages/descriptive.py
+++ frontend/pages/descriptive.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import plotly.express as px
 
@@ -6,5 +5,5 @@ from frontend.utils.dataframe import df
 
 
 # Visualise
-fig = px.line(df, x='timestamp', y= 'aqi', title='AQI Line Chart')
+fig = px.line(df, x='ts', y= 'aqi', title='AQI Line Chart')
 st.plotly_chart(fig)
