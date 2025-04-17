47dc96b0fb40268aae9d822d5e6a47b9fc47c64d
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2025-04-17 19:40:23 +0700

    Add 1-day forecast statistics

--- frontend/pages/predictive.py
+++ frontend/pages/predictive.py
@@ -10,6 +10,32 @@ oneday_tab, threedays_tab = st.tabs(["24 Hours", "72 Hours"])
 with oneday_tab:
     st.subheader("24 Hours Forecast")
 
+    # st.plotly_chart(graph.line_graph.pm_forecast1d)
+    #
+    # st.plotly_chart(graph.line_graph.aqi_forecast1d)
+    st.write("PM Forecast for the next day")
+    st.write("AQI Forecast for the next day")
+
+    forecast_1d = graph.stats.oneday_forecast
+
+    # Mean
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Average AQI", f"{forecast_1d.loc['mean', 'aqi']:.2f}")
+    col2.metric("Average PM 2.5", f"{forecast_1d.loc['mean', 'pm2_5']:.2f} µg/m³")
+    col3.metric("Average PM 10", f"{forecast_1d.loc['mean', 'pm10_0']:.2f} µg/m³")
+
+    # Min
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Minimum AQI", f"{forecast_1d.loc['min', 'aqi']:.2f}")
+    col2.metric("Minimum PM 2.5", f"{forecast_1d.loc['min', 'pm2_5']:.2f} µg/m³")
+    col3.metric("Minimum PM 10", f"{forecast_1d.loc['min', 'pm10_0']:.2f} µg/m³")
+
+    # Max
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Maximum AQI", f"{forecast_1d.loc['max', 'aqi']:.2f}")
+    col2.metric("Maximum PM 2.5", f"{forecast_1d.loc['max', 'pm2_5']:.2f} µg/m³")
+    col3.metric("Maximum PM 10", f"{forecast_1d.loc['max', 'pm10_0']:.2f} µg/m³")
+
 with threedays_tab:
 
     st.subheader("72 Hours Forecast")
