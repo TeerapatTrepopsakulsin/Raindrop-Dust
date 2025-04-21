fa9a0123c4a56b456f8a67c973de348709693ca7
patth <pattharamon.d@gmail.com>
2025-04-22 00:02:50 +0700

    improve current

--- frontend/app.py
+++ frontend/app.py
@@ -6,38 +6,55 @@ st.set_page_config(
     layout="wide",
 )
 
+st.markdown(f"""
+<style>
+    .main-title {{
+        font-size: 5em;
+    }}
+</style>
+""", unsafe_allow_html=True)
+
 rain(
     emoji="💧",
-    font_size=10,
-    falling_speed=3,
+    font_size=12,
+    falling_speed=5,
     animation_length="infinite",
 )
 
-st.title('Raindrop Dust')
+st.markdown(f"""
+<div style="
+    font-size: 5em;
+    font-weight: bold;
+    color: #94B4C1;
+    text-align: center;
+    margin-bottom: 1rem;">
+    Raindrop <span style='color:white;'>Dust</span> 🌧️
+</div>
+""", unsafe_allow_html=True)
 
 col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
 with col1:
-    st.page_link("pages/current.py", label="Current", icon="😶‍🌫️")
+    st.page_link("pages/current.py", label="Current", icon="🆕")
 with col2:
-    st.page_link("pages/exploration.py", label="Exploration", icon="🤯")
+    st.page_link("pages/exploration.py", label="Exploration", icon="🔍")
 with col3:
-    st.page_link("pages/descriptive.py", label="Descriptive", icon="🤤")
+    st.page_link("pages/descriptive.py", label="Descriptive", icon="📈")
 with col4:
-    st.page_link("pages/predictive.py", label="Predictive", icon="🥹")
+    st.page_link("pages/predictive.py", label="Predictive", icon="🔮")
 with col5:
-    st.page_link("pages/dataset.py", label="Dataset", icon="😎")
+    st.page_link("pages/dataset.py", label="Dataset", icon="🗂️")
 with col6:
-    st.page_link("pages/api.py", label="API", icon="😍")
+    st.page_link("pages/api.py", label="API", icon="🗝️")
 with col7:
-        st.page_link("pages/api_docs.py", label="Documents", icon="😷")
+        st.page_link("pages/api_docs.py", label="Documents", icon="📄")
 
-current_page = st.Page("pages/current.py", title="Current", icon="😶‍🌫️")
-exploration_page = st.Page("pages/exploration.py", title="Exploration", icon="🤯")
-descriptive_page = st.Page("pages/descriptive.py", title="Descriptive", icon="🤤")
-predictive_page = st.Page("pages/predictive.py", title="Predictive", icon="🥹")
-dataset_page = st.Page("pages/dataset.py", title="Dataset", icon="😎")
-api_page = st.Page("pages/api.py", title="API", icon="😍")
-api_docs_page = st.Page("pages/api_docs.py", title="Documents", icon="😷")
+current_page = st.Page("pages/current.py", title="Current", icon="🆕")
+exploration_page = st.Page("pages/exploration.py", title="Exploration", icon="🔍")
+descriptive_page = st.Page("pages/descriptive.py", title="Descriptive", icon="📈")
+predictive_page = st.Page("pages/predictive.py", title="Predictive", icon="🔮")
+dataset_page = st.Page("pages/dataset.py", title="Dataset", icon="🗂️")
+api_page = st.Page("pages/api.py", title="API", icon="🗝️")
+api_docs_page = st.Page("pages/api_docs.py", title="Documents", icon="📄")
 
 pg = st.navigation({
     "Visualisation": [current_page, exploration_page, descriptive_page, predictive_page],
--- frontend/pages/current.py
+++ frontend/pages/current.py
@@ -1,27 +1,24 @@
 import streamlit as st
 from frontend.components import graph
-from streamlit_extras.metric_cards import style_metric_cards
+import matplotlib.colors as mcolors
 
 head_text = "#ECEFCA"
 color_2 = "#94B4C1"
 color_3 = "#547792"
 color_4 = "#213448"
-red = "#e74c3c"
-green = "#e74c3c"
-grey = "#7f8c8d"
 
 st.markdown(f"""
 <style>
     .main-title {{
         font-size: 3em;
-        font-weight: 700;
-        color: {head_text};
+        font-weight: bold;
+        color: {color_2};
         margin-bottom: 0.3em;
     }}
 
     .section-title {{
         font-size: 1.6em;
-        font-weight: 600;
+        font-weight: bold;
         color: {color_2};
         border-bottom: 2px solid #ecf0f1;
         padding-bottom: 5px;
@@ -30,25 +27,37 @@ st.markdown(f"""
     }}
     
     div[data-testid="stMetric"] {{
-        background-color: {color_3};
-        color: {head_text}
-        padding: 10rem;
-        margin: 5px
+        background-color: {color_4};
+        padding: 1rem;
+        margin: 5px;
         border-radius: 10px;
-        backdrop-filter: blur(5px)
-        transition: .3s ease;
+        backdrop-filter: blur(5px);
+        transition: background-color 0.3s ease;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
     }}
+    
     div[data-testid="stMetric"]:hover {{
-        background-color: {color_2};
+        background-color: {color_3};
+    }}
+    
+        div[data-testid="stMetric"] > div {{
+        justify-content: center;
+        text-align: center;
     }}
 </style>
 """, unsafe_allow_html=True)
 
-
+custom_cmap = mcolors.LinearSegmentedColormap.from_list(
+    "white_to_blue", [head_text, color_2]
+)
 
 st.markdown("<div class='main-title'>Current Condition</div>", unsafe_allow_html=True)
 
-latest_tab, today_tab, week_tab = st.tabs(["📍 Latest", "📅 Today", "📈 This Week"])
+latest_tab, today_tab, week_tab = st.tabs(["📍 Latest", "⏰ Today", "📅 This Week"])
 
 with latest_tab:
     latest_df = graph.stats.latest
@@ -61,76 +70,93 @@ with latest_tab:
 
     st.markdown("<div class='section-title'>☁️ Weather Overview</div>", unsafe_allow_html=True)
     col1, col2, col3, col4 = st.columns(4)
-    col1.metric("Cloud (%)", f"{latest_df['cloud']:.2f}", f"{delta_df['cloud']:.2f}", delta_color="inverse")
-    col2.metric("Total Rainfall (mm)", f"{latest_df['rain']:.2f}", f"{delta_df['rain']:.2f}", delta_color="inverse")
-    col3.metric("Wind speed (m/s)", f"{latest_df['wind_spd']:.2f}", f"{delta_df['wind_spd']:.2f}",
-              delta_color="inverse")
+    col1.metric("Cloud (%)", f"{latest_df['cloud']:.2f}", f"{delta_df['cloud']:.2f}"
+                , border=True, help="OpenWeatherAPI")
+    col2.metric("Total Rainfall (mm)", f"{latest_df['rain']:.2f}", f"{delta_df['rain']:.2f}"
+                , border=True, help="OpenWeatherAPI")
+    col3.metric("Wind speed (m/s)", f"{latest_df['wind_spd']:.2f}", f"{delta_df['wind_spd']:.2f}"
+                , border=True, help="OpenWeatherAPI")
     col4.metric("Weather condition", f"{latest_df['weather_main']}", f"{latest_df['weather_con']}",
-              delta_color="off")
+                delta_color="off", border=True, help="OpenWeatherAPI")
 
     col1, col2, col3, col4 = st.columns(4)
     with col1:
         st.markdown("<div class='section-title'>💧 Environment</div>", unsafe_allow_html=True)
-        st.metric("Humidity (%)", f"{latest_df['hum']:.2f}", f"{delta_df['hum']:.2f}", delta_color="inverse")
-        st.metric("Light (lux)", f"{latest_df['light']:.2f}", f"{delta_df['light']:.2f}", delta_color="inverse")
-        st.metric("Air Quality Index", f"{latest_df['aqi']:.2f}", f"{delta_df['aqi']:.2f}", delta_color="inverse")
+        st.metric("Humidity (%)", f"{latest_df['hum']:.2f}", f"{delta_df['hum']:.2f}"
+                  , border=True, help="KidBright")
+        st.metric("Light (lux)", f"{latest_df['light']:.2f}", f"{delta_df['light']:.2f}"
+                  , border=True, help="KidBright")
+        st.metric("Air Quality Index", f"{latest_df['aqi']:.2f}", f"{delta_df['aqi']:.2f}"
+                  , border=True, help="KidBright")
     with col2:
         st.markdown("<div class='section-title'>🌡️ Temperature</div>", unsafe_allow_html=True)
-        st.metric("Average temperature (°C)", f"{latest_df['temp']:.2f}",
-                    f"{delta_df['temp']:.2f}", delta_color="inverse")
-        st.metric("Minimum temperature (°C)", f"{latest_df['min_temp']:.2f}",
-                    f"{delta_df['min_temp']:.2f}", delta_color="inverse")
-        st.metric("Maximum temperature (°C)", f"{latest_df['max_temp']:.2f}",
-                    f"{delta_df['max_temp']:.2f}", delta_color="inverse")
+        st.metric("Average temperature (°C)", f"{latest_df['temp']:.2f}", f"{delta_df['temp']:.2f}"
+                  , border=True, help="KidBright")
+        st.metric("Minimum temperature (°C)", f"{latest_df['min_temp']:.2f}", f"{delta_df['min_temp']:.2f}"
+                  , border=True, help="KidBright")
+        st.metric("Maximum temperature (°C)", f"{latest_df['max_temp']:.2f}", f"{delta_df['max_temp']:.2f}"
+                  , border=True, help="KidBright")
     with col3:
         st.markdown("<div class='section-title'>🌫️ PM: Atmospheric</div>", unsafe_allow_html=True)
-        st.metric("PM 1.0 (µg/m³)", f"{latest_df['pm1_0_atm']:.2f}", f"{delta_df['pm1_0_atm']:.2f}",
-                    delta_color="inverse")
-        st.metric("PM 2.5 (µg/m³)", f"{latest_df['pm2_5_atm']:.2f}", f"{delta_df['pm2_5_atm']:.2f}",
-                    delta_color="inverse")
-        st.metric("PM 10.0 (µg/m³)", f"{latest_df['pm10_0_atm']:.2f}", f"{delta_df['pm10_0_atm']:.2f}",
-                    delta_color="inverse")
+        st.metric("PM 1.0 (µg/m³)", f"{latest_df['pm1_0_atm']:.2f}", f"{delta_df['pm1_0_atm']:.2f}"
+                  , border=True, help="KidBright")
+        st.metric("PM 2.5 (µg/m³)", f"{latest_df['pm2_5_atm']:.2f}", f"{delta_df['pm2_5_atm']:.2f}"
+                  , border=True, help="KidBright")
+        st.metric("PM 10.0 (µg/m³)", f"{latest_df['pm10_0_atm']:.2f}", f"{delta_df['pm10_0_atm']:.2f}"
+                  , border=True, help="KidBright")
     with col4:
         st.markdown("<div class='section-title'>🏭 PM: Factory Sensor</div>", unsafe_allow_html=True)
-        st.metric("PM 1.0 (µg/m³)", f"{latest_df['pm1_0']:.2f}³", f"{delta_df['pm1_0']:.2f}",
-                    delta_color="inverse")
-        st.metric("PM 2.5 (µg/m³)", f"{latest_df['pm2_5']:.2f}", f"{delta_df['pm2_5']:.2f}",
-                    delta_color="inverse")
-        st.metric("PM 10.0 (µg/m³)", f"{latest_df['pm10_0']:.2f}", f"{delta_df['pm10_0']:.2f}",
-                    delta_color="inverse")
+        st.metric("PM 1.0 (µg/m³)", f"{latest_df['pm1_0']:.2f}³", f"{delta_df['pm1_0']:.2f}"
+                  , border=True, help="KidBright")
+        st.metric("PM 2.5 (µg/m³)", f"{latest_df['pm2_5']:.2f}", f"{delta_df['pm2_5']:.2f}"
+                  , border=True, help="KidBright")
+        st.metric("PM 10.0 (µg/m³)", f"{latest_df['pm10_0']:.2f}", f"{delta_df['pm10_0']:.2f}"
+                  , border=True, help="KidBright")
 
     st.markdown("<div class='section-title'>🧪 Particle Count</div>", unsafe_allow_html=True)
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.metric("Particles > 0.3 μm", f"{latest_df['pcnt_0_3']:.2f}", f"{delta_df['pcnt_0_3']:.2f}",
-                    delta_color="inverse")
-        st.metric("Particles > 0.5 μm", f"{latest_df['pcnt_0_5']:.2f}", f"{delta_df['pcnt_0_5']:.2f}",
-                    delta_color="inverse")
+        st.metric("Particles > 0.3 μm", f"{latest_df['pcnt_0_3']:.2f}", f"{delta_df['pcnt_0_3']:.2f}"
+                  , border=True, help="KidBright")
+        st.metric("Particles > 0.5 μm", f"{latest_df['pcnt_0_5']:.2f}", f"{delta_df['pcnt_0_5']:.2f}"
+                  , border=True, help="KidBright")
     with col2:
-        st.metric("Particles > 1.0 μm", f"{latest_df['pcnt_1_0']:.2f}", f"{delta_df['pcnt_1_0']:.2f}",
-                    delta_color="inverse")
-        st.metric("Particles > 2.5 μm", f"{latest_df['pcnt_2_5']:.2f}", f"{delta_df['pcnt_2_5']:.2f}",
-                    delta_color="inverse")
+        st.metric("Particles > 1.0 μm", f"{latest_df['pcnt_1_0']:.2f}", f"{delta_df['pcnt_1_0']:.2f}"
+                  , border=True, help="KidBright")
+        st.metric("Particles > 2.5 μm", f"{latest_df['pcnt_2_5']:.2f}", f"{delta_df['pcnt_2_5']:.2f}"
+                  , border=True, help="KidBright")
     with col3:
-        st.metric("Particles > 5 μm", f"{latest_df['pcnt_5_0']:.2f}", f"{delta_df['pcnt_5_0']:.2f}",
-                    delta_color="inverse")
-        st.metric("Particles > 10 μm", f"{latest_df['pcnt_10_0']:.2f}", f"{delta_df['pcnt_10_0']:.2f}",
-                    delta_color="inverse")
+        st.metric("Particles > 5 μm", f"{latest_df['pcnt_5_0']:.2f}", f"{delta_df['pcnt_5_0']:.2f}"
+                  , border=True, help="KidBright")
+        st.metric("Particles > 10 μm", f"{latest_df['pcnt_10_0']:.2f}", f"{delta_df['pcnt_10_0']:.2f}"
+                  , border=True, help="KidBright")
 
 with today_tab:
-    st.subheader("Today's Statistics")
+    st.markdown("<div class='section-title'>📊 Today's Descriptive Statistics</div>", unsafe_allow_html=True)
+
+    st.dataframe(graph.stats.today.style.background_gradient(
+        cmap=custom_cmap, axis=0
+    ).set_properties(
+        **{
+            'color': color_4,
+            'font-weight': 'bold'
+        }
+    ).format("{:.2f}"), use_container_width=True)
 
     st.plotly_chart(graph.line_graph.aqi_ts_today, use_container_width=True)
     st.plotly_chart(graph.line_graph.pm_ts_today, use_container_width=True)
 
-    st.text("Descriptive Statistics")
-    st.dataframe(graph.stats.today)
-
 with week_tab:
-    st.subheader("This Week's Statistics")
+    st.markdown("<div class='section-title'>📊 This Week's Descriptive Statistics</div>", unsafe_allow_html=True)
+
+    st.dataframe(graph.stats.week.style.background_gradient(
+        cmap=custom_cmap, axis=0
+    ).set_properties(
+        **{
+            'color': color_4,
+            'font-weight': 'bold'
+        }
+    ).format("{:.2f}"), use_container_width=True)
 
     st.plotly_chart(graph.line_graph.aqi_ts_week, use_container_width=True)
     st.plotly_chart(graph.line_graph.pm_ts_week, use_container_width=True)
-
-    st.text("Descriptive Statistics")
-    st.dataframe(graph.stats.week)
